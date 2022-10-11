4fab20975c85cb106cbe708078b3d36b899f5f35
abiyyu surya harry <111698491+kokobiyu@users.noreply.github.com>
2022-10-11 22:28:40 +0700

    Update process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -40,6 +40,13 @@ def clean_data(df):
     #df = pd.concat([df,pd.get_dummies(df.genre)],axis=1)
     #df = df.drop(['genre','social'],axis=1) 
     df = df.drop_duplicates()
+    
+    #remove empty column
+    df.drop(['child_alone'], axis=1, inplace = True) 
+    
+    #this could be an error so i replace it with '1' assuming it is valid response
+    df['related'] = df['related'].map(lambda x: 1 if x==2 else x)
+    
     return df
 
 def save_data(df, database_filename):
